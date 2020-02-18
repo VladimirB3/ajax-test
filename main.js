671a64b8ad6dabd0975e2968e761a39154d6a434
Vladimirs Boreckis <v.boreckis@gmail.com>
2020-02-18 14:50:14 +0000

    created and invoked program

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
 var xhr = new XMLHttpRequest();
+var data;
+
+xhr.open("GET", "https://swapi.co/api/");
+xhr.send();
+
+function setData(jsonData) {
+    data = jsonData;
+    console.log(data);
+}
 
 xhr.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-        document.getElementById("data").innerHTML = this.responseText;
+        setData (JSON.parse(this.responseText));
     }
 };
-
-xhr.open("GET", "https://swapi.co/api/");
-
-xhr.send();
