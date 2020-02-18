278bc9dffcdc5a0952308a256738f3a28e67d554
Vladimirs Boreckis <v.boreckis@gmail.com>
2020-02-18 15:32:46 +0000

    Timeout function added

--- main.js
+++ main.js
@@ -4,13 +4,13 @@ var data;
 xhr.open("GET", "https://swapi.co/api/");
 xhr.send();
 
-function setData(jsonData) {
-    data = jsonData;
-    console.log(data);
-}
 
 xhr.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-        setData (JSON.parse(this.responseText));
+        data = JSON.parse(this.responseText);
     }
 };
+
+setTimeout(function() {
+    console.log(data);  
+}, 800);
